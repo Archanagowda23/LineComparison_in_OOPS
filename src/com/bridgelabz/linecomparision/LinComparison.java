b4e3f52bc37709b4fc95f93b38806a4b6bcb8d7e
Archanagowda23 <archanar249@gmail.com>
2022-03-10 17:53:35 +0530

    initial commit

---
+++ src/com/bridgelabz/linecomparision/LinComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparision;
+
+public class LinComparison {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to line comparision program");
+    }
+}
